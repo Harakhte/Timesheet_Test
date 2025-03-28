1b905a105d2a236e999fc9615c17d143a9abebdc
Harakhte <nguyenxuanphuong162000@gmail.com>
2025-03-28 10:58:04 +0700

    a

--- Models/Entities/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TImesheet_TEST.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
